6c423cb3b317f50af35b03691a4192ce19e8cf29
황상윤 <s_infinite@naver.com>
2023-01-13 01:57:19 +0900

    init. babel setting

---
+++ my-babel-preset.js
@@ -0,0 +1,14 @@
+module.exports = function myBabelPreset() {
+  return {
+    // plugins: [
+    //   // const,let 을 var로 변경해 준다.
+    //   // @babel/preset-env 플러그인에 포함되어 있다.
+    //   "@babel/plugin-transform-block-scoping",
+    //   // arrow 함수를 일반 함수로 바꾸어 준다.
+    //   // @babel/preset-env 플러그인에 포함되어 있다.
+    //   "@babel/plugin-transform-arrow-functions",
+    //   // 브라우저의 엄격모드를 실행해 준다.
+    //   "@babel/plugin-transform-strict-mode",
+    // ],
+  };
+};
--- webpack.config.js
+++ webpack.config.js
@@ -38,23 +38,31 @@ module.exports = {
         // css-loader의 경우 빌드된 js 안에 css 코드를 변환하여 넣어주기만 하기때문에 후처리를 통해 html에 적용되게 설정해야된다.(css-loader만으로는 바로 적용되지 않음)
         // style-loader는 자바스크립트로 변환된 style 코드를 html로 넣어주는 로더이다.
       },
-      //! wbpack 버전4의 방식
-      // {
-      //   test: /\.(png|jpg|gif|svg)$/,
-      //   // 로더 이름 설정
-      //   loader: "url-loader",
-      //   // asset 파일들을 불러오기 위한 로더 (file-loader의 기능이 합쳐짐)
-      //   options: {
-      //     // file-loader가 처리하는 파일을 모듈로 사용했을때 경로앞에 추가되는 문자열
-      //     // output 경로가 ./dist/ 이므로 해당 문자열을 추가
-      //     //publicPath: "./dist/",
-      //     // 파일로더가 파일을 output에 복사할 때 사용하는 파일 이름을 동적으로 표시
-      //     // [원본파일명].[확장자명]?[캐시무력화를 위한 해시값]
-      //     name: "[name].[ext]?[hash]",
-      //     limit: 20000, // 20kb 이하의 용량을 base64로 변환한다. 이상일 경우 file-loader로 처리한다.
-      //   },
-      // },
-
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        loader:
+          //* 바벨 프리셋이나 플러그인을 webpack build에 적용시킨다.
+          "babel-loader", // 바벨 로더를 추가한다
+      },
+      {
+        //! wbpack 버전4의 방식
+        // {
+        //   test: /\.(png|jpg|gif|svg)$/,
+        //   // 로더 이름 설정
+        //   loader: "url-loader",
+        //   // asset 파일들을 불러오기 위한 로더 (file-loader의 기능이 합쳐짐)
+        //   options: {
+        //     // file-loader가 처리하는 파일을 모듈로 사용했을때 경로앞에 추가되는 문자열
+        //     // output 경로가 ./dist/ 이므로 해당 문자열을 추가
+        //     //publicPath: "./dist/",
+        //     // 파일로더가 파일을 output에 복사할 때 사용하는 파일 이름을 동적으로 표시
+        //     // [원본파일명].[확장자명]?[캐시무력화를 위한 해시값]
+        //     name: "[name].[ext]?[hash]",
+        //     limit: 20000, // 20kb 이하의 용량을 base64로 변환한다. 이상일 경우 file-loader로 처리한다.
+        //   },
+        // },
+      },
       //* 버전 5는 자체 설정으로 설정한다
       // asset/resource : 별도의 파일을 내보내고 URL을 추출 (file-loader 대체)
       // asset/inline : asset의 data URI를 내보낸다. (url-loader 대체)
